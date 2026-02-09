818a0c35b0f888f183f9ddf6c5fb6bf17e82320d
AMOUSSOULIFI <159581289+AMOUSSOULIFI@users.noreply.github.com>
2026-02-09 10:23:30 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,16 @@
+(function () {
+    const session = JSON.parse(localStorage.getItem("sessionData"));
+
+    if (!session || !session.isLoggedIn) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    // Vérification expiration
+    if (Date.now() > session.expiresAt) {
+        localStorage.removeItem("sessionData");
+        alert("Session expirée. Veuillez vous reconnecter.");
+        window.location.href = "index.html";
+    }
+})();
 
